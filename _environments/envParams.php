1d84b9596769668375676a26eb9bbf8f456dfc94
Harm Jacob Drijfhout <harm@spreling.nl>
2015-08-10 15:56:46 +0200

    multiple bugfixes in template

--- _environments/envParams.php
+++ _environments/envParams.php
@@ -42,8 +42,8 @@ return [
             'tests/codeception/bin/yii',
         ],
         'setCookieValidationKey' => [
-            'backend/config/main-local.php',
-            'frontend/config/main-local.php',
+            'backend/engine/config/main-local.php',
+            'frontend/engine/config/main-local.php',
         ],
     ],
     'Production' => [
@@ -58,8 +58,8 @@ return [
             'yii',
         ],
         'setCookieValidationKey' => [
-            'backend/config/main-local.php',
-            'frontend/config/main-local.php',
+            'backend/engine/config/main-local.php',
+            'frontend/engine/config/main-local.php',
         ],
     ],
 ];
