fe7932561b7d6946a577f6df0340690445a982e0
Harm Jacob Drijfhout <harm@spreling.nl>
2015-08-25 12:15:25 +0200

    Add missing main-config changes

--- frontend/core/config/main.php
+++ frontend/core/config/main.php
@@ -1,26 +1,38 @@
 <?php
 $params = array_merge(
-    require(__DIR__ . '/../../../_common/engine/config/params.php'),
-    require(__DIR__ . '/../../../_common/engine/config/params-local.php'),
+    require(__DIR__ . '/../../../_common/core/config/params.php'),
+    require(__DIR__ . '/../../../_common/core/config/params-local.php'),
     require(__DIR__ . '/params.php'),
     require(__DIR__ . '/params-local.php')
 );
 
 return [
     'id' => 'spreling-frontend',
-    'basePath' => '@frontend/engine',
+
+    //Paths uses alliases
+    'basePath' => '@frontend/core',
     'runtimePath' => '@frontend/runtime',
-    'layoutPath' => '@common/layouts',
+
+
+    //defaultRoute uses request path
+    'defaultRoute' => 'zionCore/site/index',
     'bootstrap' => ['log'],
-    //'controllerNamespace' => 'frontend\core\controllers',
+
+
+
+    //modules uses namespace
     'modules' => [
-        'core' => 'app\modules\core\coreModule'
+        'zionCore' => [
+            'class' => 'frontendModules\zionCore\ZionCoreModule',
+        ],
+        'account' => 'frontendModules\account\AccountModule',
     ],
+
     'components' => [
-//        'user' => [
-//            'identityClass' => 'common\models\User',
-//            'enableAutoLogin' => true,
-//        ],
+        'user' => [
+            'identityClass' => 'commonModules\account\models\AccountLoginLogic',
+            'enableAutoLogin' => true,
+        ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
             'targets' => [
@@ -31,8 +43,29 @@ return [
             ],
         ],
 //        'errorHandler' => [
-//            'errorAction' => 'site/error',
+//            'errorAction' => 'zionCore/site/error',
 //        ],
+        'urlManager' => [
+            'class' => 'yii\web\UrlManager',
+            // Disable index.php
+            'showScriptName' => false,
+            // Disable r= routes
+            'enablePrettyUrl' => true,
+            'rules' => array(
+                'index' => '/zionCore/site/',
+                'about' => '/zionCore/site/about',
+                'contact' => '/zionCore/site/contact',
+                'login' => '/account/account/login',
+                'logout' => '/account/account/logout',
+                'signup' => '/account/account/signup',
+                'login/reset' => '/account/account/request-password-reset',
+                'login/reset-password/<token:.+>' => '/account/account/reset-password',
+
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+            ),
+        ],
     ],
     'params' => $params,
 ];
